c4f32a94173e8ad0c6b6c91368ca36dc85769cca
Abi180798 <habiburrahman180798@gmail.com>
2019-07-21 09:13:59 +0800

    testBot-can talk

--- app/src/main/java/com/example/testbot/MainActivity.java
+++ app/src/main/java/com/example/testbot/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.testbot;
 
 import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Locale;
 
 import ai.api.AIListener;
 import ai.api.android.AIConfiguration;
@@ -18,24 +39,72 @@ import ai.api.model.Result;
 
 public class MainActivity extends AppCompatActivity implements AIListener {
     AIService aiService;
-    Button btn;
-    TextView hasil;
+    Button btn, btn2, btn3;
+    TextView hasil,hasil1;
     private static final int RECORD_REQUEST_CODE = 101;
+    private TextToSpeech textToSpeech;
+    private SeekBar mSeekBarPitch;
+    private SeekBar mSeekBarSpeed;
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         int permission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.RECORD_AUDIO);
 
         if (permission != PackageManager.PERMISSION_GRANTED) {
             Log.i("permissionn", "Permission to record denied");
-            makeRequest();
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Permission to access the microphone is required for this app to record audio.")
+                        .setTitle("Permission required");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.i("clic", "Clicked");
+                        makeRequest();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                makeRequest();
+            }
         }
 
         btn = findViewById(R.id.btn);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
         hasil = findViewById(R.id.hasil);
+        hasil1 = findViewById(R.id.hasil1);
+        mSeekBarPitch = findViewById(R.id.seek_bar_pitch);
+        mSeekBarSpeed = findViewById(R.id.seek_bar_speed);
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS){
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                    int result = textToSpeech.setLanguage(Locale.GERMAN);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        btn2.setEnabled(true);
+                    }
+                }else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
 
         final AIConfiguration config = new AIConfiguration("6195941710814115b441d85391a59668",
                 AIConfiguration.SupportedLanguages.English,
@@ -44,12 +113,61 @@ public class MainActivity extends AppCompatActivity implements AIListener {
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
 
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                aiService.startListening();
-//            }
-//        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+                Log.d("dooi",hasil1.getText().toString());
+//                speak();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                speak();
+            }
+        });
+    }
+
+    private void speak(String s) {
+        String text = s;
+//        Log.d("textx", text);
+        float pitch = (float) mSeekBarPitch.getProgress() / 50;
+        if (pitch < 0.1) pitch = 0.1f;
+        float speed = (float) mSeekBarSpeed.getProgress() / 50;
+        if (speed < 0.1) speed = 0.1f;
+
+        textToSpeech.setPitch(pitch);
+        textToSpeech.setSpeechRate(speed);
+
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+
+        super.onDestroy();
+    }
+
+    private void promptSpeechInput() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Say Something");
+        try {
+            startActivityForResult(intent, RECORD_REQUEST_CODE);
+
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Your device doesn\\'t support speech input",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     protected void makeRequest() {
@@ -57,6 +175,113 @@ public class MainActivity extends AppCompatActivity implements AIListener {
                 new String[]{Manifest.permission.RECORD_AUDIO},
                 RECORD_REQUEST_CODE);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RECORD_REQUEST_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String userQuery=result.get(0);
+                    hasil.setText(userQuery);
+                    Log.d("userquery", result.get(0));
+                    RetrieveFeedTask task=new RetrieveFeedTask();
+                    task.execute(userQuery);
+                    Log.d("twtwtw", hasil1.getText().toString());
+
+                }
+                break;
+            }
+
+        }
+    }
+
+    public String GetText(String query) throws UnsupportedEncodingException {
+
+        String text = "";
+        BufferedReader reader = null;
+
+        // Send data
+        try {
+
+            // Defined URL  where to send data
+            URL url = new URL("https://api.api.ai/v1/query?v=20150910");
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            conn.setRequestProperty("Authorization", "Bearer 6195941710814115b441d85391a59668");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            //Create JSONObject here
+            JSONObject jsonParam = new JSONObject();
+            JSONArray queryArray = new JSONArray();
+            queryArray.put(query);
+            jsonParam.put("query", queryArray);
+//            jsonParam.put("name", "order a medium pizza");
+            jsonParam.put("lang", "en");
+            jsonParam.put("sessionId", "1234567890");
+
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            Log.d("karma", "after conversion is " + jsonParam.toString());
+            wr.write(jsonParam.toString());
+            wr.flush();
+            Log.d("karma", "json is " + jsonParam);
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+
+            // Read Server Response
+            while ((line = reader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+
+
+
+            JSONObject object1 = new JSONObject(text);
+            JSONObject object = object1.getJSONObject("result");
+            JSONObject fulfillment = null;
+            String speech = null;
+//            if (object.has("fulfillment")) {
+            fulfillment = object.getJSONObject("fulfillment");
+//                if (fulfillment.has("speech")) {
+            speech = fulfillment.optString("speech");
+//                }
+//            }
+            str = speech;
+
+            Log.d("karma ", "response is " + text);
+            return speech;
+
+        } catch (Exception ex) {
+            Log.d("karma", "exception at last " + ex);
+        } finally {
+            try {
+
+                reader.close();
+            } catch (Exception ex) {
+            }
+        }
+
+        return null;
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -82,6 +307,7 @@ public class MainActivity extends AppCompatActivity implements AIListener {
         Log.d("resultnya", result.toString());
         Result result1 = result.getResult();
         hasil.setText("Query= " + result1.getResolvedQuery() + " Action= " + result1.getAction());
+        Log.d("hasill", result1.getAction());
     }
 
     @Override
@@ -111,5 +337,33 @@ public class MainActivity extends AppCompatActivity implements AIListener {
 
     public void btnClick(View view) {
         aiService.startListening();
+    }
+
+    class RetrieveFeedTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... voids) {
+            String s = null;
+            try {
+
+                s = GetText(voids[0]);
+                Log.d("hasils", s);
+                speak(s);
+
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                Log.d("karma", "Exception occurred " + e);
+            }
+
+            return s;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            hasil1.setText(s);
+
+        }
     }
 }
